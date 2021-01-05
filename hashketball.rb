47d9d49f860f0e5087cb8c0fd346bf299d0afe5b
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 01:38:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,19 +61,19 @@ game_hash = {
         }]
   }
   :away {
-    :team_name => ""
-    :colors => ["",""]
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise","Purple"]
     :players => [
       {
-        :player_name
-        :number
-        :shoe
-        :points
-        :rebounds
-        :assists
-        :steals
-        :blocks
-        :slam_dunks
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
         },
       {
 
