8df0b17b61e7308bb610ca2cb41631f08113cf35
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 01:47:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
 {
-  :home {
+  :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black","White"],
     :players => [
@@ -61,7 +61,7 @@ def game_hash
         :slam_dunks => 1
         }]
   }
-  :away {
+  :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise","Purple"]
     :players => [
